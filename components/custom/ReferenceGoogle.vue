27ce1a7664c3990aeb2cffe36581c87932b0add9
Sukhtseren Tsetsegee <sukusovi@gmail.com>
2026-06-01 10:30:08 +0200

    Update.

--- components/custom/ReferenceGoogle.vue
+++ components/custom/ReferenceGoogle.vue
@@ -10,11 +10,11 @@ const references: { user: UserProps; quote: string }[] = [
       avatar: {
         src: "https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/acidjazz",
         srcset: "https://ipx.nuxt.com/f_auto,s_80x80/gh_avatar/acidjazz 2x",
-        loading: "lazy" as const,
+        loading: "lazy",
       },
     },
     quote:
-      "Nuxt UI Pro saves 100s of hours of dev and design time while delivering a clean professional look on any device.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     user: {
@@ -23,11 +23,11 @@ const references: { user: UserProps; quote: string }[] = [
       avatar: {
         src: "https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/mokkapps",
         srcset: "https://ipx.nuxt.com/f_auto,s_80x80/gh_avatar/mokkapps 2x",
-        loading: "lazy" as const,
+        loading: "lazy",
       },
     },
     quote:
-      "I decided to replace my custom-built components with a component library and chose Nuxt UI Pro. It only took me a few hours, and the new UI looks more professional. Integrating the library is easy; the components are well-documented and highly customizable. I can only recommend it; this library is my new choice for new SaaS products.",
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
   {
     user: {
@@ -36,11 +36,11 @@ const references: { user: UserProps; quote: string }[] = [
       avatar: {
         src: "https://ipx.nuxt.com/f_auto,s_40x40/gh_avatar/harlan-zw",
         srcset: "https://ipx.nuxt.com/f_auto,s_80x80/gh_avatar/harlan-zw 2x",
-        loading: "lazy" as const,
+        loading: "lazy",
       },
     },
     quote:
-      "Nuxt UI Pro is my go to component library. Out-of-the-box it handles all of the UI demands I throw at it while looking great. The customisation is really worth thought out, allowing you to override components in a breeze. Always amazed at the improvements dropped in each update as well, the team is doing an amazing job.",
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur pretium tincidunt lacus nec vehicula.",
   },
   {
     user: {
@@ -48,11 +48,11 @@ const references: { user: UserProps; quote: string }[] = [
       description: "YouTuber and SaaS builder",
       avatar: {
         src: "https://pbs.twimg.com/profile_images/1607353032420769793/I8qQSUfQ_400x400.jpg",
-        loading: "lazy" as const,
+        loading: "lazy",
       },
     },
     quote:
-      "Nuxt UI has allowed me to develop my SaaS without any prior mockups. The design quality of their components and the intelligence of the DX meant that I was able to try many different layouts for my application until I found the perfect UX for my users. Nuxt UI is the ui-kit I would have dreamed of building myself, and Nuxt UI Pro makes things even easier when you want to go further with your SaaS. Kudos to the team.",
+      "Nulla facilisi. Maecenas vel enim sit amet orci tincidunt rhoncus. Vivamus lacinia odio vitae vestibulum vestibulum. Donec in efficitur leo, in commodo odio.",
   },
 ];
 
@@ -60,10 +60,6 @@ const selectedIndex = ref<number | null>(null);
 const selectedReference = computed(() => {
   return references[selectedIndex.value ?? 0];
 });
-
-function onCardClick(index: number) {
-  selectedIndex.value = index;
-}
 </script>
 
 <template>
@@ -104,7 +100,7 @@ function onCardClick(index: number) {
             'before:content-[open-quote] after:content-[close-quote] line-clamp-3',
         }"
         class="w-64 shrink-0 cursor-pointer"
-        @click="onCardClick(index)"
+        @click="selectedIndex = index"
       >
         <template #footer>
           <UUser
